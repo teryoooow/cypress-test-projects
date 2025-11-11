f450202e073974997d4f03d782dbaf6221da6196
teryow <sarmientoterelle@gmail.com>
2025-11-11 16:20:47 +0800

    continuation of multiwindow test with gemini

--- cypress/e2e/concepts/multiwindow.cy.js
+++ cypress/e2e/concepts/multiwindow.cy.js
@@ -1,4 +1,3 @@
-import { it } from "mocha";
 import { multiwindowObjects } from "../../pageObjects/objects";
 
 describe('multiwindow tests', () => {
@@ -6,13 +5,25 @@ describe('multiwindow tests', () => {
         cy.visit(multiwindowObjects.multiwindowPageUrl);
     });
 
-    it('QUICKLINKS_001	Open link in new tab', () => {
-        cy.get(multiwindowObjects.basicButton).click();
+    it('QUICKLINKS_001    Open link in new tab', () => {
+        cy.get(multiwindowObjects.learnAboutButtonLink).find('a').invoke('attr', 'href').then(href => {
+            cy.visit(href);
+        });
+        cy.url().should('include', '/concepts/button');
+        // Assuming the button page has some unique content to verify
+        cy.contains('Follow').should('be.visible'); 
     });
 
-    it('QUICKLINKS_002	Open link in new window', () => {
-        cy.get(multiwindowObjects.basicButton).click();
+    it('QUICKLINKS_002    Open link in new window', () => {
+        cy.window().then((win) => {
+            cy.stub(win, 'open').as('windowOpen');
+        });
+        cy.get(multiwindowObjects.learnAboutTimePickerLink).find('a').click();
+        cy.get('@windowOpen').should('be.calledWith', '/concepts/timepicker', '_blank', 'width=800,height=600');
     });
 
-
+    it('QUICKLINKS_003 Broken or missing navigation', () => {
+        cy.visit('/concepts/xyz', { failOnStatusCode: false });
+        cy.get('body').should('contain', '404 - Page Not Found');
+    });
 });
